5711fd92bc6fe946068ad5cd2b93d054005994d9
vsrschool83-sketch <vsrschool83@gmail.com>
2026-03-15 13:36:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,41 +3,42 @@ const axios = require("axios");
 
 const app = express();
 
-const firebaseURL = "https://esp32gps-firebase-tracker-default-rtdb.asia-southeast1.firebasedatabase.app/gps.json";
-
-app.get("/gps", async (req, res) => {
-
-    const lat = req.query.lat;
-    const lng = req.query.lng;
-
-    if (!lat || !lng) {
-        return res.send("Missing coordinates");
-    }
-
-    try {
-
-        await axios.put(firebaseURL, {
-            latitude: lat,
-            longitude: lng,
-            time: Date.now()
-        });
-
-        res.send("GPS Updated");
-
-    } catch (error) {
-
-        res.status(500).send("Firebase error");
-
-    }
-
+app.get("/", (req,res)=>{
+  res.send("GPS API Running");
 });
 
-app.get("/", (req,res)=>{
-    res.send("ESP32 GPS API Running");
+app.get("/gps", async (req,res)=>{
+
+  const lat = req.query.lat;
+  const lng = req.query.lng;
+
+  if(!lat || !lng){
+    return res.send("Missing coordinates");
+  }
+
+  try{
+
+    const firebaseURL = "https://esp32gps-firebase-tracker-default-rtdb.asia-southeast1.firebasedatabase.app/gps.json";
+
+    await axios.put(firebaseURL,{
+      latitude:lat,
+      longitude:lng,
+      time:Date.now()
+    });
+
+    res.send("GPS Updated");
+
+  }
+  catch(error){
+
+    res.status(500).send("Firebase error");
+
+  }
+
 });
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
+app.listen(PORT,()=>{
+  console.log("Server started");
 });
