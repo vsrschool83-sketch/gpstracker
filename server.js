fda3dd528bf3697dd9c5c2dcb6baa8a936b6f3d2
vsrschool83-sketch <vsrschool83@gmail.com>
2026-03-15 14:05:55 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,44 +1,23 @@
 const express = require("express");
-const axios = require("axios");
 
 const app = express();
 
 app.get("/", (req,res)=>{
-  res.send("GPS API Running");
+    res.send("GPS API Running");
 });
 
-app.get("/gps", async (req,res)=>{
+app.get("/gps",(req,res)=>{
+    const lat=req.query.lat;
+    const lng=req.query.lng;
 
-  const lat = req.query.lat;
-  const lng = req.query.lng;
+    console.log("Latitude:",lat);
+    console.log("Longitude:",lng);
 
-  if(!lat || !lng){
-    return res.send("Missing coordinates");
-  }
-
-  try{
-
-    const firebaseURL = "https://esp32gps-firebase-tracker-default-rtdb.asia-southeast1.firebasedatabase.app/gps.json";
-
-    await axios.put(firebaseURL,{
-      latitude:lat,
-      longitude:lng,
-      time:Date.now()
-    });
-
-    res.send("GPS Updated");
-
-  }
-  catch(error){
-
-    res.status(500).send("Firebase error");
-
-  }
-
+    res.send("GPS Received");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,()=>{
-  console.log("Server started");
+    console.log("Server running on port "+PORT);
 });
